22c5ff4b424d3b327f6bfdb722233bd7c14052d1
Denis <mdr2010@yandex.ru>
2022-12-24 09:27:50 +0500

    add Seminars

---
+++ Seminars/Seminar002/Exes011/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+int a = new Random().Next(100, 1000);
+Console.WriteLine(a);
+int a1 = a / 100;
+int a2 = a % 10;
+Console.WriteLine($"{a1}{a2}");
+
+// или Console.Write($"{a/100}{a%10}");
+// или int b = (num1 % 100) % 10;
