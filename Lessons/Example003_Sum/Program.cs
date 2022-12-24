69064f29e3de487c4e09aed07fef5cdcfaba1b24
Denis <mdr2010@yandex.ru>
2022-12-24 09:27:05 +0500

    add Lessons

---
+++ Lessons/Example003_Sum/Program.cs
@@ -0,0 +1,15 @@
+﻿int numberA = 3;
+int numberB = 5;
+Console.WriteLine(numberA + numberB);
+
+int numberC = 31;
+int numberD = 5;
+int result = numberC + numberD;
+Console.WriteLine(result);
+
+int numberE = new Random().Next(1,10); // 1 2 3...9 Случайные числа от 1 до 10
+Console.Write(numberE);
+int numberF = new Random().Next(1,10);
+Console.WriteLine(numberF);
+int sult = numberE + numberF;
+Console.WriteLine(sult);
