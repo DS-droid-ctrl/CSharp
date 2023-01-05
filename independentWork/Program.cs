b2c637d069a5880acf4f9a457ec70a1516dba118
DenisM <mdr2010@yandex.ru>
2023-01-05 21:09:04 +0500

    Add independent work

---
+++ independentWork/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+static void formatString()
+{
+    //Console.OutputEncoding = Encoding.UTF8;
+    double money = 12.8;
+    //Console.WriteLine(money.ToString("C2"));
+    Console.WriteLine($"{money:C2}");
+}
+
+static void sravnenieString()
+{
+    
+}
