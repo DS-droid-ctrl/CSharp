5fccf7aa4e201e54a6f56e8ff23712061d7942d2
DenisM <mdr2010@yandex.ru>
2023-01-16 01:08:13 +0500

    Add seminars

--- Seminars/Seminar005/Task32/Program.cs
+++ Seminars/Seminar005/Task32/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int [] Array = CreateRandomArray(123, -10, 123);
+ShowArray(Array);
+int count = 0;
+
+ for (int i = 0; i< Array.Length; i++)
+{
+    if (Array[i]>=10 && Array[i]<=99)
+    {
+        count = count +1;
+    }
+}
+
+Console.WriteLine($"В массиве ровно {count} чисел в диапазоне между 10 и 99");
