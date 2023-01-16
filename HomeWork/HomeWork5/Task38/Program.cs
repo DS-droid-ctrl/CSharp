4a461b9180c93880e37da50e74691d1ebe3460f6
DenisM <mdr2010@yandex.ru>
2023-01-16 23:04:15 +0500

    Add HomeWork

--- HomeWork/HomeWork5/Task38/Program.cs
+++ HomeWork/HomeWork5/Task38/Program.cs
@@ -2,3 +2,32 @@
 // Найдите разницу между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
 
+Console.Clear();
+float[] CreateRandomArray(int N, float start, float end)
+{
+    float[] RandomArray = new float[N];
+    for (int i = 0; i < N; i++)
+    {
+        RandomArray[i] = new Random().NextFloat(start, end + 1);
+    }
+    return RandomArray;
+}
+
+int num = 6;
+int min = -100;
+int max = 999;
+
+float[] myRandomArray = CreateRandomArray(num, min, max);
+Console.WriteLine($" [ {String.Join("| ", myRandomArray)} ] ");
+
+// генерируем и запоняем
+
+// const int maxLenght = 50;
+// static void Main()
+// {
+// Random Rand = new Random();
+// double[ ] ms = new double[maxLenght];
+// for (int i = 0; i < ms.Length; i++) ms[ i ] = Rand.NextDouble();
+// foreach (double d in ms) Console.WriteLine(d);
+// Console.ReadKey(true);
+// }
