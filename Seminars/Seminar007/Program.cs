d2771cdf299df452648bf98010ab7ce982e06ec8
DenisM <mdr2010@yandex.ru>
2023-01-22 21:36:07 +0500

    add seminars

---
+++ Seminars/Seminar007/Program.cs
@@ -0,0 +1,28 @@
+﻿void PrintArray(int[,] matr)
+    {
+        for (int i = 0; i < 3; i++)  // или for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < 4; j++) // или for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write($"{matr[i, j]} "); // выводим в одну строку
+            }
+            Console.WriteLine(); // пустая строка между выводимыми между таблицами
+        }
+    }
+
+    void FillArray(int[,] matr) // функция заполнения двумерного массива числами
+    {
+        for (int i = 0; i < matr.GetLength(0); i++)
+        {
+            for (int j = 0; j < matr.GetLength(1); j++)
+            {
+                matr[i, j] = new Random().Next(1, 10);
+            }
+        }
+    }
+
+    int[,] matrix = new int[3, 4];
+    PrintArray(matrix);
+    FillArray(matrix);
+    Console.WriteLine();
+    PrintArray(matrix);
