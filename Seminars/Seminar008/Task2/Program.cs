1d2ffbb745a3c5243ebeda7d25f9a42a99b47b1a
DenisM <mdr2010@yandex.ru>
2023-01-28 14:15:39 +0500

    add seminar8

---
+++ Seminars/Seminar008/Task2/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива
+Console.Clear();
+int[,] matrix = new int[5, 5];
+
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+Console.WriteLine();
+ChangeRow(matrix);
+PrintArray(matrix);
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void ChangeRow(int[,] matr)
+{
+    int temp = 0;
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        temp = matr[0, i];
+        matr[0, i] = matr[matr.GetLength(0)-1, i];
+        matr[matr.GetLength(0)-1, i] = temp;
+    }
+}
---
+++ Seminars/Seminar008/Task55/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. 
+// В случае, если это невозможно, программа должна вывести сообщение для пользователя
+Console.Clear();
+int[,] matrix = new int[5, 5];
+
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+Console.WriteLine();
+Transposition(matrix);
+PrintArray(matrix);
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void Transposition(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+        for (int j = 0; j < matr.GetLength(1); j++)
+            matr[i, j] = matr[j, i];
+    matr[j, i] = matr[i, j];
+}
