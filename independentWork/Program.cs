61b516c46f8996f388b762ac03206848192a590b
DenisM <mdr2010@yandex.ru>
2023-01-30 01:01:46 +0500

    add independentWork

--- independentWork/Program.cs
+++ independentWork/Program.cs
@@ -33,7 +33,107 @@ static void sravnenieString()
 // string sentence = $"Your age is {age}";
 // Console.WriteLine(sentence);
 
-Console.BackgroundColor = ConsoleColor.Cyan;
-Console.ForegroundColor = ConsoleColor.DarkMagenta;
-Console.Write("New Style ");
-Console.Write("New Style\n"); // \n это тоже самое, что WriteLine
+static void FormatColor()
+{
+    Console.BackgroundColor = ConsoleColor.Cyan;
+    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+    Console.Write("New Style ");
+    Console.Write("New Style\n"); // \n это тоже самое, что WriteLine
+}
+
+// РЕКУРСИЯ
+
+static void FactorialNumbers()
+{
+    Console.Clear();
+    int factorial(int n)
+    {
+        if (n == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return n * factorial(n - 1);
+        }
+    }
+    // Console.WriteLine("Введите число, факториал которого хотите найти: ");
+    // int n = Convert.ToInt32(Console.ReadLine());
+    // Console.WriteLine(factorial(n));
+
+    Console.WriteLine(factorial(5));
+}
+
+static void BeskonechayaRekursiya()
+{/*
+    // бесконечная рекурсия, печатающая 1
+    void rec(int x)
+    {
+        Console.WriteLine(x);
+        rec(x);
+    }
+    rec(1);
+
+    // бесконечная рекурсия с подсчетом строк
+    void rec(int x)
+    {
+        Console.WriteLine(x);
+        rec(x + 1); // подсчет количества строк
+    }
+    rec(1);
+
+    // ОБРАТНЫЙ ОТСЧЕТ
+    void rec(int x)
+    {
+        if (x < 4)
+        {
+            // Console.WriteLine(x);
+            rec(x + 1);
+            Console.WriteLine(x);
+        }
+
+    }
+    rec(1);*/
+}
+
+static void ChisloFibonacci()
+{
+    Console.Clear();
+    int fibonacci(int n)
+    {
+        if (n == 1)
+        {
+            return 0;
+        }
+        if (n == 2)
+        {
+            return 1;
+        }
+        else
+        {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+    // Console.WriteLine("Введите число, факториал которого хотите найти: ");
+    // int n = Convert.ToInt32(Console.ReadLine());
+    // Console.WriteLine(fibonacci(n));
+
+    Console.WriteLine(fibonacci(5));
+}
+
+void polindrom (string s = "шалаш")
+{
+    if (s.Length <= 1)
+    {
+        Console.WriteLine("Полиндром");
+    }
+    if (s[0] != s[s.Length-1])
+    {
+        Console.WriteLine("Не полиндром");
+    }
+    else
+    {
+        return polindrom(s[s.Length-s.Length+1; s.Length-1])
+    }    
+}
+
