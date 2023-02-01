6dc58fd640674cac9301eb9357eb14442d50715c
DenisM <mdr2010@yandex.ru>
2023-02-02 00:19:01 +0500

    add homwork

---
+++ HomeWork/HomeWork8/Task54/Program.cs
@@ -0,0 +1,79 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива
+
+Console.Clear();
+Console.WriteLine("Введите размер матрицы:");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[a, b];
+
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+FillArrayReplace(matrix);
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+// void FillArrayReplace(int[,] matr)
+// {
+//     int max = 0;
+//     for (int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matr.GetLength(1)-1; j++)
+//         {
+//             if (matrix[i, j] > matrix[i, j+1]) { max = matrix[i, j]; }
+//             else { max = matrix[i, j+1]; matrix[i, j+1] = matrix[i, j]; matrix[i, j] = max;}
+//             Console.WriteLine(matrix);
+//         }
+//         //matrix[i, j] = max;
+//         //onsole.WriteLine(FillArrayReplace);
+//     }
+//     //Console.WriteLine(FillArrayReplace);
+// }
+
+void FillArrayReplace(int[,] matr)
+{
+// Console.WriteLine("---");
+//сортируем
+for (int i = 0; i < matrix.GetLength(0); i++)
+    //сортировка пузырьком
+    for (int j = 0; j < matrix.GetLength(1); j++)
+        for (int k = 0; k < matrix.GetLength(1); k++)
+        {
+            if (matrix[i, j] <= matrix[i, k]) continue;
+            int temp = matrix[i, j];
+            matrix[i, j] = matrix[i, k];
+            matrix[i, k] = temp;
+            Console.Write(matrix[i, j]);
+        }
+        
+
+// for (int i = 0; i < matrix.GetLength(0); i++, Console.WriteLine())
+//     for (int j = 0; j < matrix.GetLength(1); j++)
+//     {
+//         Console.Write($"{0,4}", matrix[i, j]);
+//     }
+
+// FillArrayReplace(matrix);
+// Console.Read();
+}
---
+++ HomeWork/HomeWork8/Task56/Program.cs
@@ -0,0 +1,52 @@
+﻿// Task56. Задайте прямоугольный двумерный массив. Напишите
+// программу, которая будет находить строку с наименьшей суммой элементов
+
+Console.Clear();
+Console.WriteLine("Введите размер двумерного массива через пробел:");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[a, b];
+
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+//FillArrayReplace(matrix);
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+int[,] MultiMatrix(int[,] arrayA, int[,] arrayB)
+{
+    int[,] arrayC = new int[arrayA.GetLength(0), arrayB.GetLength(1)];
+    for (int i = 0; i < arrayA.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayB.GetLength(1); j++)
+        {
+            for (int k = 0; k < arrayA.GetLength(1); k++)
+            {
+                arrayC[i, j] += arrayA[i, k] * arrayB[k, j];
+            }
+        }
+    }
+    return arrayC;
+}
